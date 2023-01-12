7d94b156172b330c69ad120b33319ebf2b5b1fbe
Yaroslav.S <yar921@yandex.ru>
2023-01-12 11:10:39 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,11 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("19.01.2020");
+        int age = 18;
+        System.out.println(age);
+        float lenght = 78.0332F;
+        System.out.println(lenght);
+        boolean baby = true;
+        System.out.println(baby);
     }
 }
