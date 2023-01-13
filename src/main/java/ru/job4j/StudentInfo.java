73ef630481566d0997220542561590a2b23263c0
Yaroslav.S <yar921@yandex.ru>
2023-01-13 15:25:47 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Petr Arsentev");
-        System.out.println("06.01.2019");
-        System.out.println("1 * 2 = 2");
-    }
-}
--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,25 +2,21 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public static int plus1(int first, int second) {
-        int result1 = first + second;
-        return result1;
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 
-    public static int plus2(int first, int second) {
-        int result2 = first + second;
-        return result2;
-    }
-
-    public static int func1(int x) {
-        return x;
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-    int onePlusTwo = Calculator.plus1(1, 2);
-    int thenPlusEleven = Calculator.plus2(10, 11);
-    int func1 = Calculator.func1(100);
-    int result3 = onePlusTwo + thenPlusEleven + func1;
-        System.out.println(result3);
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
     }
+
 }
