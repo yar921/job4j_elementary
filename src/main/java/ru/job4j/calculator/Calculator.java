df2d29a62d7fb3d0c2d3ce2b34be0a72be2d1b8c
Yaroslav.S <yar921@yandex.ru>
2023-01-13 14:24:12 +0300

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,26 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public static int plus1(int first, int second) {
+        int result1 = first + second;
+        return result1;
+    }
+
+    public static int plus2(int first, int second) {
+        int result2 = first + second;
+        return result2;
+    }
+
+    public static int func1(int x) {
+        return x;
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int five = 5;
-        int four = 4;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+    int onePlusTwo = Calculator.plus1(1, 2);
+    int thenPlusEleven = Calculator.plus2(10, 11);
+    int func1 = Calculator.func1(100);
+    int result3 = onePlusTwo + thenPlusEleven + func1;
+        System.out.println(result3);
     }
 }
---
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int result1 = MathFunc.func1(100);
+        int result2 = MathFunc.func2(100);
+        int result3 = result1 + result2;
+        System.out.println(result3);
+    }
+}
