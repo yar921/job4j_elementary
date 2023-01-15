c16cc001b84782d13bf73ac3258b54fdc6e081ef
Yaroslav.S <yar921@yandex.ru>
2023-01-15 17:29:05 +0300

    4.1. Операторы ветвлений [#3611]

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void possibleDiv(int number) {
+        if (number < 0) {
+            System.out.println("This is negative number");
+        }
+    }
+
+    public static void main(String[] args) {
+        AlertDivByZero.possibleDiv(4);
+        AlertDivByZero.possibleDiv(0);
+    }
+}
